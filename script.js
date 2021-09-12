0d7ebeaebae2ce309585f366ef620e6e8406bddb
Aaron Donelson <aaron.donelson23@gmail.com>
2021-09-12 14:50:25 -0400

    start of script.js and index.html

---
+++ script.js
@@ -0,0 +1,16 @@
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+
+var firstQuestion = "Try to answer the following code-related questions within the time limit.</br> Keep in mind that incorrect answers will penalize your score.time by ten seconds!"
+
+var secondQuestion = "Commonly used data types DO NOT include:"
+// ol 1. strings, 2. booleans, 3. alerts, 4. numbers
+var thirdQuestion = "The condition in an if/else statement is enclosed within ______"
+// ol 1. quotes, 2. curly brackets, 3. parentheses, 4. square brackets
+var fourthQuestion = "Arrays in JavaScript can be used to store _______."
+// ol 1. numbers and strings, 2. other arrays, 3. booleans, 4. all of the above
+var fifthQuestion = "String values must be enclosed within ____ when being assigned to variables"
+// ol 1. commas, 2. curly brackets, 3. quotes, 4. parentheses
+var sixthQuestion = "A very useful tool used during development and debugging for printing content to the debuggger is:"
+// ol 1. JavaScript, 2. terminal/bash, 3. for loops, 4. console.log
+
